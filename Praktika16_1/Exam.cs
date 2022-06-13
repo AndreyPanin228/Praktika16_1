2d807a4b3d87fe4dda0f439b539743a4c927a70b
1 <1@DESKTOP-RF1EUM1>
2022-06-13 22:47:56 +0300

    Добавьте файлы проекта.

---
+++ Praktika16_1/Exam.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Praktika16_1
+{
+    public class Exam
+    {
+        public static bool ExamFirst(string slovo)
+        {
+            if (slovo != string.Empty)
+            {
+                for (int i = 0; i < slovo.Length; i++)
+                {
+                    if (char.IsDigit(slovo[i]))
+                    {
+                        return true;
+                    }
+                }
+            }
+            else
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static string First(string slovo)
+        {
+            StreamReader sr = new StreamReader("test.txt");
+            string[] mass = sr.ReadToEnd().Split(' ', '.', ',');
+            int count = (from i in mass where i.ToLower() == slovo select i).Count();
+            sr.Close();
+            string str = $"Ваше слово {slovo} встретилось в файле {count} раз";
+            return str;
+
+        }
+    }
+}
---
+++ Praktika16_1/Program.cs
@@ -0,0 +1,12 @@
+﻿using Praktika16_1;
+
+Console.WriteLine("Введите слово, которое хотите найти(с маленькой буквы)");
+string slovo = Console.ReadLine().ToLower();
+if (Exam.ExamFirst(slovo) == false)
+{
+    Console.WriteLine(Exam.First(slovo));
+}
+else
+{
+    Console.WriteLine("Ошибка");
+}
